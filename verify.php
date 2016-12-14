7f42f2080f8ee092eb20b6f3f3ca4d9a18a99018
NaPat1912 <family-_-changlao_01@hotmail.com>
2016-12-14 21:25:37 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '1350031035';
+$access_token = 'I+DEh81jpSlolMCbAASYPBm9C3F77jKr4NlFA9jtpNwtnbRvay7pvYyzfJz8RwelACPrQ+T30KvpfDdYQNs+SQYxDb1ew5Hg1i8eERvgWBJsiOfoNztwztmBIKC/2CjpU3e0Q2Zr4Opyc2HeJmmjLAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
