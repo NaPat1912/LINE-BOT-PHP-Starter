98cd83c9015cfafbebcab040442bbd6a70e16935
NaPat1912 <family-_-changlao_01@hotmail.com>
2016-12-16 14:59:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,15 +9,7 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('P16B/i7iP6NyxMmL2cDfXPeOy0n0V3KrqBXcs1cVSpQ+IZVCs8aajyDbB/JlctMyACPrQ+T30KvpfDdYQNs+SQYxDb1ew5Hg1i8eERvgWBJVM8vPqlPrUpqkVB366JNWUp+lHe4Mqu0qvAynWAR/aQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e6cd474bb7bb5368e1bc3ea48e92521c']);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดี');
-$response = $bot->replyMessage('ดีจ้า ยินดีให้ความช่วยเหลือ', $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-/*if (!is_null($events['events'])) {
+if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
@@ -38,7 +30,7 @@ echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
-			];*/
+			];
 			
 			
 			$post = json_encode($data);
