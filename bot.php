e3b4493bec7065c04c6c5f2725631c000d12960c
NaPat1912 <family-_-changlao_01@hotmail.com>
2016-12-16 20:14:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,16 +3,11 @@ $access_token = 'P16B/i7iP6NyxMmL2cDfXPeOy0n0V3KrqBXcs1cVSpQ+IZVCs8aajyDbB/JlctM
 $proxy = 'http://fixie:FWwieAEjnTaoGI4@velodrome.usefixie.com:80';
 $proxyauth = 'http://fixie:FWwieAEjnTaoGI4@velodrome.usefixie.com:80';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('P16B/i7iP6NyxMmL2cDfXPeOy0n0V3KrqBXcs1cVSpQ+IZVCs8aajyDbB/JlctMyACPrQ+T30KvpfDdYQNs+SQYxDb1ew5Hg1i8eERvgWBJVM8vPqlPrUpqkVB366JNWUp+lHe4Mqu0qvAynWAR/aQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e6cd474bb7bb5368e1bc3ea48e92521c']);
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-
-$response = $bot->replyText('<reply token>', 'hello');
 
 if (!is_null($events['events'])) {
 	// Loop through each event
