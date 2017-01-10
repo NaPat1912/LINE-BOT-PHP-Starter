1da8ffa346a39003a74b92d31ecd41489ad1439f
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-10 16:14:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,42 +3,54 @@ $access_token = 'P16B/i7iP6NyxMmL2cDfXPeOy0n0V3KrqBXcs1cVSpQ+IZVCs8aajyDbB/JlctM
 $proxy = 'http://fixie:FWwieAEjnTaoGI4@velodrome.usefixie.com:80';
 $proxyauth = 'http://fixie:FWwieAEjnTaoGI4@velodrome.usefixie.com:80';
 
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
 // Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-		$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
-		echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-		/*if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];*/
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$response],
-			];
+$post = file_get_contents('php://input');
+$urlReply = 'https://api.line.me/v2/bot/message/reply';
+		function postMessage($token,$packet,$urlReply){
+ 		$dataEncode = json_encode($packet);
+ 		$headersOption = array(‘Content-Type: application/json’,’Authorization: Bearer ‘.$token);
+	
+			{“events”:[{“type”:”message”,”replyToken”:”ไม่บอก”,”source”:
+				   {“userId”:”ไม่บอก”,”type”:”user”},”timestamp”:1477132643802,
+				    ”message”:{“type”:”text”,”id”:”5094630491076",”text”:”ว่าไงท่าน”}}]}
+			$res = json_decode($post, true);
+			if(isset($res[‘events’]) && !is_null($res[‘events’])){
+ 				foreach($res[‘events’] as $item){
+ 				if($item[‘type’] == ‘message’){
+ 					switch($item[‘message’][‘type’]){
+ 					case ‘text’:
+					break;
+					case ‘image’:
+					break;
+ 					case ‘video’:
+ 
+ 					break;
+ 					case ‘audio’:
+ 
+ 					break;
+ 					case ‘location’:
+					break;
+ 					case ‘sticker’:
+
+ 					break;
+					}
+		function getSticker($replyToken){
+ 		$sticker = array(
+ 		‘type’ => ‘sticker’,
+ 		‘packageId’ => ‘4’,
+ 		‘stickerId’ => ‘300’
+ 		);
+ 		$packet = array(
+ 		‘replyToken’ => $replyToken,
+ 		‘messages’ => array($sticker),
+ 		);
+ 		return $packet;
+		}
+		
+		$packet = getSticker($item[‘replyToken’]);
+ 		postMessage($token,$packet,$urlReply);
 			
-			
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			
-			
-			$ch = curl_init($url);
+			$ch = curl_init($urlReply);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
