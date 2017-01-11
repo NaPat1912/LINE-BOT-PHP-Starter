d94dc0a325eccb2b843df33a9a530dd96e7db1d1
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-11 10:36:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == "สวัสดี") {
+		if ($event['events'] == "สวัสดี") {
 			// Get text sent
 			$text = "ดีจ้า ยินดีต้อนรับ^_^";
 			// Get replyToken
