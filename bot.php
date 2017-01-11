53c04ea0ee001a3ae5865807d6da22bf3c97ac6a
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-11 11:14:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -47,6 +47,42 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 			;echo $result . "\r\n";
 		}
+		else if ($event['message']['text'] == "เก็บรูป") {
+			// Get text sent
+			$text = "กรุณาเลือกโฟลเดอร์";
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+				
+			];
+			
+			
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+			
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			;echo $result . "\r\n";
+		}
+		
 		else
 		{
 			$text = "ไม่ทราบข้อมูล";
