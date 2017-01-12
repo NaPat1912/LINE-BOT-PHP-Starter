bee619b855b5ccb5673f9a88ca972ad713a76c2c
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-12 11:36:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,10 +21,10 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [{
 				'type' => 'text',
-				'text' => $text};
+				'text' => $text},
 				     {
 				'type' => 'text',
-				'text' => "Test"};
+				'text' => "Test"}
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
