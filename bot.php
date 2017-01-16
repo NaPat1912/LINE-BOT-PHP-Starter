4fd91ef8ce37ec8ce8a8bf882e91dbb2987be66a
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-16 11:23:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,6 @@ if (!is_null($events['events'])) {
 		if ($event['message']['text'] == "สวัสดี"||$event['message']['text'] == "หวัดดี") {
 			// Get text sent
 			$text = "ดีจ้า ยินดีต้อนรับ^_^ \nมีอะไรให้เราช่วยมั้ย???";
-			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
@@ -86,7 +85,6 @@ if (!is_null($events['events'])) {
 				'type' => 'image',
 				'image' => $image
 				];
-			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
