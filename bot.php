43deedf5528b1253b37ac7e611856d9bb4a61f6d
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-17 16:07:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,9 +7,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-"events":["type":"message","timestamp":12345678901234,"source":{"type":"user","groupId":"userid"},
-   "replyToken":"replytoken","message":{"id":"contentid","type":"image"}
-  if (!is_null($events['events'])) {
+if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
@@ -75,14 +73,34 @@ $events = json_decode($content, true);
 			;echo $result . "\r\n";
 		}
 		else if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
-			$event = $events[1];
-            		$this->assertTrue($event->isUserEvent());
-            		$this->assertEquals('groupid', $event->getUserId());
-            		$this->assertEquals('groupid', $event->getEventSourceId());
-            		$this->assertInstanceOf('LINE\LINEBot\Event\MessageEvent\ImageMessage', $event);
-            		/** @var ImageMessage $event */
-            		$this->assertEquals('replytoken', $event->getReplyToken());
-            		$this->assertEquals('image', $event->getMessageType());
+			// Get text sent
+			$image = $event['message']['image'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Build message to reply back
+			
+			$messages = [
+				'type' => 'image',
+				'image' => $image
+				];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+				];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
 			;echo $result . "\r\n";
 		}
 		else if($event['message']['type'] == 'sticker') 
