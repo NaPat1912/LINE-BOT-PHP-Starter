7e27b104739f3be13e55c1a6d12d07d3ae2df640
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-19 14:53:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -119,11 +119,11 @@ if (!is_null($events['events'])) {
 		}
 		else if($event['message']['type'] == 'sticker') 
 		{
-			$sticker = $event['type' => 'sticker','packageId' => '4','stickerId' => '300'];
+			$sticker = $event['sticker']['4']['300'];
 			$replyToken = $event['replyToken'];
 			$messages = [
-				'type' => 'text',
-				'text' => $sticker
+				'type' => 'sticker',
+				'sticker' => $sticker
 			];
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
