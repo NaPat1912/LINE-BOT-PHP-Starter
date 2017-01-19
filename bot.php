ee6d3bd292498497cab9d35eff237c02e2013b83
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-19 17:40:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -76,8 +76,8 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = array(
 				"type"=>"image",
-    				"originalContentUrl"=>"https://aisapi.herokuapp.com/P4160012/original.jpg",
-    				"previewImageUrl"=>"https://aisapi.herokuapp.com/P4160012/preview.jpg");
+    				"originalContentUrl"=>"https://aisapi.herokuapp.com/P4160012.jpg",
+    				"previewImageUrl"=>"https://aisapi.herokuapp.com/P4160012.jpg");
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Make a POST Request to Messaging API to reply to sender
