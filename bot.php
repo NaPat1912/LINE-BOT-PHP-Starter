deb42ef31c8b75efac48b0d8f1a67ed3c6881b25
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-19 15:14:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -119,7 +119,7 @@ if (!is_null($events['events'])) {
 		}
 		else if($event['message']['type'] == 'sticker') 
 		{
-			$sticker = $event['sticker','packageId'=>'4','stickerId'=>'293'];
+			$sticker = $event['type'=>'sticker']['packageId'=>'4']['stickerId'=>'293'];
 			$replyToken = $event['replyToken'];
 			$messages = [
 				'type' => 'sticker',
