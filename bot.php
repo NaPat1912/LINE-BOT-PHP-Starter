d48eed88b8ee6465c4eae3dcb065f1a0255d0267
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-23 16:19:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,6 @@
 <?php
 $access_token = 'P16B/i7iP6NyxMmL2cDfXPeOy0n0V3KrqBXcs1cVSpQ+IZVCs8aajyDbB/JlctMyACPrQ+T30KvpfDdYQNs+SQYxDb1ew5Hg1i8eERvgWBJVM8vPqlPrUpqkVB366JNWUp+lHe4Mqu0qvAynWAR/aQdB04t89/1O/w1cDnyilFU=';
+$messageId = '"uf6834da6efac06e194055a6348780f4f"';
 $proxy = 'http://fixie:FWwieAEjnTaoGI4@velodrome.usefixie.com:80';
 $proxyauth = 'http://fixie:FWwieAEjnTaoGI4@velodrome.usefixie.com:80';
 // Get POST body content
@@ -76,10 +77,10 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 			;echo $result . "\r\n";
 		}
-		/*else if ($event['message']['type'] == 'image') {
+		else if ($event['message']['type'] == 'image') {
 			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('P16B/i7iP6NyxMmL2cDfXPeOy0n0V3KrqBXcs1cVSpQ+IZVCs8aajyDbB/JlctMyACPrQ+T30KvpfDdYQNs+SQYxDb1ew5Hg1i8eERvgWBJVM8vPqlPrUpqkVB366JNWUp+lHe4Mqu0qvAynWAR/aQdB04t89/1O/w1cDnyilFU=');
 			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e6cd474bb7bb5368e1bc3ea48e92521c']);
-			$response = $bot->getMessageContent('uf6834da6efac06e194055a6348780f4f');
+			$response = $bot->$content('$messageId');
 				if ($response->isSucceeded()) {
    				$tempfile = tmpfile();
     				fwrite($tempfile, $response->getRawBody());
@@ -87,7 +88,7 @@ if (!is_null($events['events'])) {
     				error_log(echo "ERROR");
 				}
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/uf6834da6efac06e194055a6348780f4f/content';
+			$url = 'https://api.line.me/v2/bot/message/{messageId}/content';
 			$post = json_encode($tempfile);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$ch = curl_init($url);
@@ -101,7 +102,7 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 			;echo $result . "\r\n";
-		}*/
+		}
 		else if ($event['message']['text'] == "ขอดูรูปหน่อย") {
 			// Get text sent
 			$text = array(
