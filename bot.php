03b3429bcf53f4458f6793148eab7bbac4b5e356
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-23 17:59:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,9 +9,6 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
@@ -77,11 +74,9 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 			;echo $result . "\r\n";
 		}
-		else if ($event['message']['type'] == 'image') {
-			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('P16B/i7iP6NyxMmL2cDfXPeOy0n0V3KrqBXcs1cVSpQ+IZVCs8aajyDbB/JlctMyACPrQ+T30KvpfDdYQNs+SQYxDb1ew5Hg1i8eERvgWBJVM8vPqlPrUpqkVB366JNWUp+lHe4Mqu0qvAynWAR/aQdB04t89/1O/w1cDnyilFU=');
-			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e6cd474bb7bb5368e1bc3ea48e92521c']);
-			$response = $bot->getMessageContent('$messageId');
-				if ($response->isSucceeded()) {
+		/*else if ($event['message']['type'] == 'image') {
+			$response = ['message']['image'];
+				if ($response == isSucceeded()) {
    				$tempfile = tmpfile();
     				fwrite($tempfile, $response->getRawBody());
 				} else {
@@ -102,7 +97,7 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 			;echo $result . "\r\n";
-		}
+		}*/
 		else if ($event['message']['text'] == "ขอดูรูปหน่อย") {
 			// Get text sent
 			$text = array(
