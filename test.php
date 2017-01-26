465c2a5048e76c476b87e306e9650b04b364997b
NaPat1912 <family-_-changlao_01@hotmail.com>
2017-01-26 15:58:55 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-$file = fopen("https://github.com/NaPat1912/LINE-BOT-PHP-Starter/test.txt","w");
+$file = fopen("https://aisapi.herokuapp.com/test.txt","w");
 echo fwrite($file,"Hello World. Testing!");
 fclose($file);
 
